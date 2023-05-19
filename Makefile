b2728862ad34f86fd99cc56f5cd121ee5755826e
Alexandra Burtseva <allburtseva111@gmail.com>
2023-05-19 17:03:39 +0300

    add brain-prime

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ brain-gcd:
 brain-progression:
 	node bin/brain-progression.js
 
+brain-prime:
+	node bin/brain-prime.js
+
 brain-games:
 	node bin/brain-games.js
 
---
+++ src/games/brain-prime.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import {
+  compareAnswers,
+  getRandomNum,
+  startGame,
+} from '../index.js';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const startRound = () => {
+  const number = getRandomNum(2, 100);
+  console.log(`Question: ${number}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  if (compareAnswers(userAnswer, correctAnswer)) {
+    console.log('Correct!');
+    return true;
+  } console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
+
+const runGame = () => {
+  startGame(startRound, 'Answer "yes" if given number is prime. Otherwise answer "no".');
+};
+
+export default runGame;
