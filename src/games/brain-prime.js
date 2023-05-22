651af9d97371cb99a056839902d4273aa008abba
Alexandra <126781946+allburtseva@users.noreply.github.com>
2023-05-22 13:45:17 +0300

    Update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,9 +1,12 @@
 import {
-  compareAnswers,
   getRandomNum,
   startGame,
-  getAnswer,
-} from '../index.js';
+  } from '../index.js';
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const min = 2;
+const max = 100;
 
 const isPrime = (number) => {
   for (let i = 2; i <= number / 2; i += 1) {
@@ -14,20 +17,13 @@ const isPrime = (number) => {
   return true;
 };
 
-const startRound = () => {
-  const number = getRandomNum(2, 100);
-  console.log(`Question: ${number}`);
-  const userAnswer = getAnswer();
+const getGameData = () => {
+  const number = getRandomNum(min, max);
+  const question = number;
   const correctAnswer = isPrime(number) ? 'yes' : 'no';
-  if (compareAnswers(userAnswer, correctAnswer)) {
-    console.log('Correct!');
-    return true;
-  } console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return false;
+  return [question, correctAnswer];
 };
 
-const runGame = () => {
-  startGame(startRound, 'Answer "yes" if given number is prime. Otherwise answer "no".');
+export default () => {
+  startGame(task, getGameData);
 };
-
-export default runGame;
