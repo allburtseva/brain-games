f70eef52fbcbf47071f0f6a0c03f56b13f80a914
Alexandra <126781946+allburtseva@users.noreply.github.com>
2023-05-22 13:49:05 +0300

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,39 +1,32 @@
 import {
-  compareAnswers,
-  getAnswer,
   getRandomNum,
   startGame,
-} from '../index.js';
+  } from '../index.js';
+
+const task = 'What number is missing in the progression?';
 
 const getProgression = () => {
   const length = 10;
-  const first = getRandomNum(1, 100);
+  const firstNumber = getRandomNum(1, 100);
   const step = getRandomNum(2, 15);
   const result = [];
-  result[0] = first;
+  result[0] = firstNumber;
   for (let i = 1; i <= length; i += 1) {
     result[i] = result[i - 1] + step;
   }
   return result;
 };
 
-const startRound = () => {
+const getGameData = () => {
   const progression = getProgression();
   const position = getRandomNum(0, 10);
-  const correctAnswer = progression[position];
+  const correctAnswer = String(progression[position]);
   const newProgression = progression;
   newProgression[position] = '..';
-  console.log(`Question: ${newProgression.join(' ')}`);
-  const userAnswer = Number(getAnswer());
-  if (compareAnswers(userAnswer, correctAnswer)) {
-    console.log('Correct!');
-    return true;
-  } console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return false;
+  const question = `${newProgression.join(' ')}`;
+  return [question, correctAnswer];
 };
 
-const runGame = () => {
-  startGame(startRound, 'What number is missing in the progression?');
+export default () => {
+  startGame(task, getGameData);
 };
-
-export default runGame;
